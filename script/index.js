7ef0a06ea305368bc3cf003208adac6d436ed892
Anastasiya Fedotova <nastenaf@list.ru>
2022-04-12 18:35:01 +0300

    1 ревью

--- script/index.js
+++ script/index.js
@@ -10,7 +10,7 @@ const profileResearcher = profile.querySelector('.profile__researcher');
 const photos = document.querySelector('.photos');
 const photosList = photos.querySelector('.photos__list');
 const photosAddButton = profile.querySelector('.profile__add-button');
-const photosElement = document.querySelector('#template-photos').content;
+const templatePhotos = document.querySelector('#template-photos').content;
 
 //Popup
 const popup = document.querySelector('.popup');
@@ -66,3 +66,100 @@ const initialCards = [
   }
 ];
 
+//Функция добавления карточек
+function addTemplatePhotos(name, URL, where = 'append') {
+  const photosCard = templatePhotos.cloneNode(true);
+  const photosImage = photosCard.querySelector('.photos__image');
+  const photosLikeButton = photosCard.querySelector('.photos__like-button');
+  const photosDeleteButton = photosCard.querySelector('.photos__delete-button');
+  photosCard.querySelector('.photos__image').src = URL;
+  photosCard.querySelector('.photos__image').alt = 'фотография ' + name;
+  photosCard.querySelector('.photos__title').textContent = name;
+  photosDeleteButton.addEventListener('click', deletePhotoButton);
+  photosImage.addEventListener('click', openPhoto);
+  photosLikeButton.addEventListener('click', likePhoto);
+  if (where === 'append') {
+    photosList.append(photosCard);
+  } else if (where === 'prepend') {
+    photosList.prepend(photosCard);
+  }
+}
+
+function initializePhotos(arr) {
+  arr.forEach(elem => {
+    addTemplatePhotos(elem.name, elem.link, 'append');
+  });
+}
+function initializeProfileInfo() {
+  inputProfileName.value = profileName.textContent;
+  inputProfileAbout.value = profileResearcher.textContent;
+}
+
+function emptyInputValue(...inputs) {
+  inputs.forEach(elem => elem.value = '');
+}
+
+function addPhotosCard(evt) {
+  evt.preventDefault();
+  addTemplatePhotos(inputPhotoName.value, inputPhotoLink.value, 'prepend');
+  emptyInputValue(inputPhotoName, inputPhotoLink);
+  closePopupAdd()
+}
+
+function openPopupEdit() {
+  initializeProfileInfo();
+  popupEdit.classList.add('popup_opened');
+}
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup_opened');
+}
+
+function closePopupEdit() {
+  popupEdit.classList.remove('popup_opened');
+}
+
+function closePopupAdd() {
+  emptyInputValue(inputPhotoName, inputPhotoLink);
+  popupAdd.classList.remove('popup_opened');
+}
+
+function profileSaveForm(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputProfileName.value;
+  profileResearcher.textContent = inputProfileAbout.value;
+  closePopupEdit();
+}
+
+//Лайк фотографий
+function likePhoto(evt) {
+  evt.target.classList.toggle('photos__like-button_liked');
+}
+
+function deletePhotoButton(evt) {
+  evt.target.closest('.photos__card').remove();
+}
+function closePhoto() {
+  popupPhotos.classList.remove('popup_opened');
+}
+
+function openPhoto(evt) {
+  const figure = evt.path[1];
+  const img = figure.querySelector('.photos__image');
+  const title = figure.querySelector('.photos__title');
+  popupPhotosBigSizeImage.src= img.src;
+  popupPhotosCaption.textContent = title.textContent;
+  popupPhotos.classList.add('popup_opened');
+}
+
+//Обработчик событий
+profileEditButton.addEventListener('click', openPopupEdit);
+popupEditCloseIcon.addEventListener('click', closePopupEdit);
+editForm.addEventListener('submit', profileSaveForm);
+
+photosAddButton.addEventListener('click', openPopupAdd);
+popupAddCloseIcon.addEventListener('click', closePopupAdd);
+addForm.addEventListener('submit', addPhotosCard);
+popupPhotosCloseIcon.addEventListener('click', closePhoto);
+
+initializePhotos(initialCards);
